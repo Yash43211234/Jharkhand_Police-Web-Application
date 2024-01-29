e5551bfe88288040efc92e8609643fef0d819040
garimaakashyap <garimakashyap2121@gmail.com>
2024-01-29 12:50:08 +0530

    new folder CawIns.js added

---
+++ src/components/CawIns.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CawIns = () => {
+  return (
+    <div>
+      <h1> this is new folder </h1>
+    </div>
+  );
+};
+
+export default CawIns;
