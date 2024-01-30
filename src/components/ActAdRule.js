aa7a038a1888f7d98bc59352da899cc4fc29998b
garimaakashyap <garimakashyap2121@gmail.com>
2024-01-30 15:34:28 +0530

    testing

---
+++ src/components/ActAdRule.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ActAdRule = () => {
+  return (
+    <div>
+      <h1> this is new folder </h1>
+      <h2> same here </h2>
+    </div>
+  );
+};
+
+export default ActAdRule;
